1a8beff85d1aaad1231ac7e45649f4e4d12e6d75
lsy <renemver@naver.com>
2017-06-19 13:40:30 +0900

    first commit check

---
+++ app/src/main/java/com/example/administrator/ekapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.ekapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//check about github
+public class MainActivity extends AppCompatActivity {
+//extension
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
